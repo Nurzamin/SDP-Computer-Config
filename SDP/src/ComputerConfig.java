b4f7f6fac0d06c0947f896d68f55c2f180ab33e8
nnurz <boyr07964@gmail.com>
2026-09-13 21:32:02 +0500

    ...

---
+++ SDP/src/ComputerConfig.java
@@ -0,0 +1,16 @@
+public class ComputerConfig {
+    private String cpu;
+    private int PSU_wattage;
+    private int ram;
+    private StorageType storage;
+    private CoolingType coolingType;
+    private String gpu;
+    private String motherboard;
+    private boolean wifi_module;
+    private boolean bluetooth_module;
+    private int warranty_years;
+
+    public ComputerConfig(Builder builder){
+
+    }
+}
